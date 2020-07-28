bacf4f8d5cb67e575817c8d444deae5378820dda
Alistair Willis <alistairrwillis@gmail.com>
2020-07-28 12:32:27 +0100

    Updates

---
+++ precache-manifest.8d5319fc0c06f39273b1e625040a9639.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7771f2698c37f2b73e0244e54df9a168",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "065a80daa59444f3aab1",
+    "url": "/react-calculator/static/css/main.c7c1ff6e.chunk.css"
+  },
+  {
+    "revision": "0c4a1836bda924e7f262",
+    "url": "/react-calculator/static/js/2.9005c141.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-calculator/static/js/2.9005c141.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "065a80daa59444f3aab1",
+    "url": "/react-calculator/static/js/main.bee35f08.chunk.js"
+  },
+  {
+    "revision": "3c4ee116adfa9d8605e9",
+    "url": "/react-calculator/static/js/runtime-main.5a7a9823.js"
+  }
+]);
